e62582d431fce53afb6a522a57d1ad55714448ca
unknown <meryemmouhim123@gmail.com>
2025-12-26 15:09:23 +0100

    Pattern Adapter par Heritage

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,6 @@
 import computer.*;
 import computer.adapter.HdmiVgaAdapter;
+import computer.adapter.HdmiVgaAdapterH;
 
 public class Test {
     public static void main(String[] args) {
@@ -8,10 +9,17 @@ public class Test {
         uniteCental.print("Bonjour");
         uniteCental.setVga(new VideoProjecteur());
         uniteCental.print("Bonjour");
+
+        //Composition
         HdmiVgaAdapter hdmiVgaAdapter=new HdmiVgaAdapter();
         hdmiVgaAdapter.setHdmi(new TV());
         uniteCental.setVga(hdmiVgaAdapter);
         hdmiVgaAdapter.print("Bonsoir Master");
+
+        //Heritage
+        HdmiVgaAdapterH hdmiVgaAdapterH=new HdmiVgaAdapterH();
+        uniteCental.setVga(hdmiVgaAdapterH);
+        hdmiVgaAdapterH.print("Test");
 
         uniteCental.setVga(new SuperVP());
         uniteCental.print("Hello");
---
+++ src/main/java/computer/adapter/HdmiVgaAdapterH.java
@@ -0,0 +1,20 @@
+package computer.adapter;
+
+import computer.Hdmi;
+import computer.TV;
+import computer.Vga;
+
+public class HdmiVgaAdapterH extends TV implements Vga {
+    private Hdmi hdmi;
+    @Override
+    public void print(String message) {
+        System.out.println("=======Adapter===========");
+        byte[] data=message.getBytes();
+        super.view(data);
+        System.out.println("=======Adapter===========");
+
+
+    }
+
+
+}
